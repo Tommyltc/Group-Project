2272dc9831496f735bca562fb97cc302ce42b830
Brian <s576kks@gmail.com>
2019-04-14 02:39:53 +0800

    flickr get more photo info

--- components/Elements/FlickrElement.jsx
+++ components/Elements/FlickrElement.jsx
@@ -8,13 +8,14 @@ export default class FlickrElement extends PureComponent {
   }
 
   render() {
-    const { title, src, datetime } = this.props.data;
+    const { title, description, src, datetime } = this.props.data;
 
     return (
       <Card style={{ width: "18rem" }}>
         <Card.Img variant="top" src={src} />
         <Card.Body>
           <Card.Title>{title}</Card.Title>
+          <Card.Subtitle>{description}</Card.Subtitle>
         </Card.Body>
         <Card.Footer className="small p-1 pl-3 bg-info text-white"><b>Flickr</b> on {moment(datetime).format("YYYY-MM-DD")}</Card.Footer>
       </Card>
--- models/flickr.js
+++ models/flickr.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
+// APP config
+const key = "d5bdef52f659cb9e8f4a43fe668b3765";
+const secret = "f4247377f9653ed7";
+
+// API setting
+const url = "https://api.flickr.com/services/rest/";
+
 export default async function getData(keyword) {
-  // APP config
-  const key = "d5bdef52f659cb9e8f4a43fe668b3765";
-  const secret = "f4247377f9653ed7";
-
-  // API setting
-  const url = "https://api.flickr.com/services/rest/";
-
   // API paramters
   let params = {};
   params = {
@@ -36,22 +36,49 @@ export default async function getData(keyword) {
 
   let result = [];
 
-  data.photos.photo.map((gp, i) => {
+  data.photos.photo.map(async (gp, i) => {
     const farmId = gp.farm;
     const serverId = gp.server;
     const id = gp.id;
     const secret = gp.secret;
+
+    const info = await getPhotoInfo(id);
 
     //console.log(farmId + ", " + serverId + ", " + id + ", " + secret);
 
     //  https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
 
     result.push({
+      _photo_id: id,
       src: `https://farm${farmId}.staticflickr.com/${serverId}/${id}_${secret}.jpg`,
-      title: gp.title,
-      datetime: "2019-04-13 00:00:00",  //hard-code first, to-do
+      title: info.title,
+      description: info.description,
+      datetime: info.datetime
     });
   });
-
+  // console.log(result);
   return result;
 }
+
+const getPhotoInfo = async id => {
+  let params = {};
+  params = {
+    method: "flickr.photos.getInfo", // https://www.flickr.com/services/api/flickr.photos.getInfo.html
+    api_key: key, // Your API application key
+    photo_id: id,
+    format: "json",
+    nojsoncallback: 1
+  };
+
+  const response = await axios.get(url, { params });
+
+  const { photo } = response.data;
+  // console.log(id);
+
+  return {
+    title: photo.title._content,
+    description: photo.description._content,
+    datetime: photo.dates.posted
+  };
+};
+
