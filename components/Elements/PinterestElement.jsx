0fc1bad36b92f68fc247afa8388ccb53c7e02e4c
Tai Lam <tai.lam@innoverz.com>
2019-04-14 14:34:39 +0800

    PinterestModal

--- components/Elements/PinterestElement.jsx
+++ components/Elements/PinterestElement.jsx
@@ -1,19 +1,30 @@
-import { PureComponent } from "react";
+import { PureComponent, Fragment } from "react";
 import { Card } from "react-bootstrap";
 import moment from "moment";
+
+import PinterestModal from "../Modals/PinterestModal";
 
 export default class PinterestElement extends PureComponent {
   constructor(props) {
     super(props);
+
+    this.state = { modalShow: false };
   }
 
   render() {
     const { title, description, photo, datetime } = this.props.data;
 
+    let modalClose = () => this.setState({ modalShow: false });
+
     return (
-      <>
+      <Fragment>
         <Card className="element pinterest">
-          <Card.Img variant="top" src={photo} />
+          <Card.Img
+            variant="top"
+            src={photo}
+            onClick={() => this.setState({ modalShow: true })}
+            style={{ cursor: "pointer" }}
+          />
           <Card.Body>
             <Card.Title>{title}</Card.Title>
             <Card.Subtitle>{description}</Card.Subtitle>
@@ -25,7 +36,13 @@ export default class PinterestElement extends PureComponent {
             <b>Pinterest</b> on {moment(datetime).fromNow()}
           </Card.Footer>
         </Card>
-      </>
+
+        <PinterestModal
+          show={this.state.modalShow}
+          onHide={modalClose}
+          imgSrc={photo}
+        />
+      </Fragment>
     );
   }
 }
